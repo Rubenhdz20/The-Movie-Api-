9e232077f30447f84b2120229f0cd319497a9255
Rubenhdz20 <rubhernandez42@gmail.com>
2024-08-20 19:49:10 -0600

    Finish search results style and start mobile menu styles

--- my-movie-app/src/components/MobileMenu/MobileMenu.jsx
+++ my-movie-app/src/components/MobileMenu/MobileMenu.jsx
@@ -2,7 +2,27 @@
 function MobileMenu() {
     return(
         <>
-            <div></div>
+            <section className="p-9 bg-background text-white">
+                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M31.6666 10.6833L29.3166 8.33334L19.9999 17.65L10.6833 8.33334L8.33325 10.6833L17.6499 20L8.33325 29.3167L10.6833 31.6667L19.9999 22.35L29.3166 31.6667L31.6666 29.3167L22.3499 20L31.6666 10.6833Z" fill="#1CE783"/>
+                </svg>
+                <nav>
+                    <ul className="flex flex-col content-center justify-center">
+                        <li>
+                            <a href="#">HOME</a>
+                        </li>
+                        <li>
+                            <a href="#">TRENDING</a>
+                        </li>
+                        <li>
+                            <a href="#">TOP RATED</a>
+                        </li>
+                        <li>
+                            <a href="#">UPCOMING</a>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
         </>
     )
 }
--- my-movie-app/src/routes/index.jsx
+++ my-movie-app/src/routes/index.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Header from '../components/Header/Header';
-import Section from '../components/Section';
-import Footer from '../components/Footer/Footer';
 import MobileMenu from '../components/MobileMenu/MobileMenu'
-import Categories from '../pages/Categories/Categories';
-import SearchBar from '../components/SearchBar';
 import MovieDetails from '../pages/MoviesDetails/index';
 import SearchResults from '../pages/SearchResults/index';
 import Layout from '../layout/index';
@@ -15,13 +10,6 @@ const router = createBrowserRouter([
     {
       path: '/',
       element: <Layout />,
-      children: [
-        { path: 'header', element: <Header /> }, 
-        { path: 'section', element: <SearchBar /> },
-        { path: 'section', element: <Section /> },
-        { path: 'categories', element: <Categories /> },
-        { path: 'footer', element: <Footer /> },
-      ],
     },
     {
       path:"/movie/:id",
