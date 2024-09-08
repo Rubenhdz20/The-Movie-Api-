d40ffd63423b056c9d5f510cda8a3b2c64e14a3e
Rubenhdz20 <rubhernandez42@gmail.com>
2024-09-07 18:26:48 -0600

    Change pahts

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'The-Movie-Api-',
+  base: '/The-Movie-Api-',
   build: {
     outDir: 'dist',
   },
